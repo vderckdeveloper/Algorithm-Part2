663937f7575e5b0dc7b9463a56ab1b5dfd6b1329
vderckdeveloper <vderckdeveloper@gmail.com>
2025-11-07 12:33:46 +0900

    feat: sequential number algorithm

---
+++ sequentialNumber.js
@@ -0,0 +1,19 @@
+const elements = [7, 9, 1, 1, 4];
+
+const solution = elements => {
+    const arr = elements.concat(elements);
+    const set = new Set();
+
+    for (let i = 0; i < elements.length; i++) {
+        for (let j = 1; j <= elements.length; len++) {
+            let sum = 0;
+            for (let k = 0; k < j; k++) {
+                sum += arr[i + k];
+            }
+            set.add(sum);
+        }
+    }
+    return set.size;
+}
+
+solution(elements);
